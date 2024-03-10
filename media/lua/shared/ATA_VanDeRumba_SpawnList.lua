3212d33d6346c4fc6a83060c504d1f2dc4935c60
redyyu <redy.ru@gmail.com>
2024-03-10 19:03:32 +0800

    change textures

--- media/lua/shared/ATA_VanDeRumba_SpawnList.lua
+++ media/lua/shared/ATA_VanDeRumba_SpawnList.lua
@@ -2,10 +2,12 @@ require "TsarSpawnList"
 
 VehicleZoneDistribution = VehicleZoneDistribution or {};
 
-VehicleZoneDistribution.parkingstall.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.trailerpark.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.junkyard.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.mccoy.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.fossoil.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.parkingstall.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 2}
+VehicleZoneDistribution.trailerpark.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 2}
+VehicleZoneDistribution.junkyard.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 1}
+VehicleZoneDistribution.mccoy.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 1}
+VehicleZoneDistribution.fossoil.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 1}
+VehicleZoneDistribution.spiffo.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 20}
 
-VehicleZoneDistribution.spiffo.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 20};
+VehicleZoneDistribution.medium.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 2}
+VehicleZoneDistribution.good.vehicles["Base.ATA_VanDeRumba"] = {index = -1, spawnChance = 1}
