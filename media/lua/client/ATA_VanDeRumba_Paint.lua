ae00aa77175a1681d4ee87b869e6ee73ec4d5516
redyyu <redy.ru@gmail.com>
2024-03-17 13:57:49 +0800

    xxx

---
+++ media/lua/client/ATA_VanDeRumba_Paint.lua
@@ -0,0 +1,77 @@
+require "PaintVehicle.lua"
+
+local vehiclePaintTable = {
+    ["Base.ATA_VanDeRumba"] = {
+        ["White"] = {
+            skinIndex = 0,
+            paintTable = {
+                ["PaintWhite"] = 5,
+            }
+        },
+        ["ChillSummer"] = {
+            skinIndex = 1,
+            paintTable = {
+                ["PaintBlack"] = 3,
+                ["PaintPink"] = 1,
+                ["PaintYellow"] = 1,
+            }
+        },
+        ["SweetSummer"] = {
+            skinIndex = 2,
+            paintTable = {
+                ["PaintRed"] = 3,
+                ["PaintPink"] = 1,
+                ["PaintYellow"] = 1,
+            }
+        },
+        ["FoundParadise"] = {
+            skinIndex = 3,
+            paintTable = {
+                ["PaintWhite"] = 1,
+                ["PaintPink"] = 1,
+                ["PaintYellow"] = 3,
+            }
+        },
+    }
+}
+
+
+local function doFillMenuOutsideVehicle(playerObj, context, vehicle, test)
+    local playerInv = playerObj:getInventory()
+    local paintPlanTable = vehiclePaintTable[vehicle:getScriptName()]
+
+    if paintPlanTable then
+        
+        local paintMenuOpt, paintSubMenu = PaintVehicle.getOrCreatePaintMenu(context)
+        local menuAvailable = false
+        
+        for k, v in pairs(paintPlanTable) do
+            if (vehicle:getSkinIndex()) ~= v.skinIndex then
+                optAvailable = PaintVehicle.createPaintMenuOpt(paintSubMenu,
+                                                               playerObj,
+                                                               context,
+                                                               vehicle,
+                                                               v.skinIndex,
+                                                               getText("ContextMenu_Paint_"..k),
+                                                               v.paintTable)
+                if optAvailable then
+                    -- any opt available is good to go.
+                    menuAvailable = true
+                end
+            end
+        end
+
+        if not menuAvailable then
+            paintMenuOpt.notAvailable = true
+        end
+
+    end
+end
+
+
+local function onfillMenuOutsideVehicleMenu(player, context, worldobjects, test)
+    PaintVehicle.doPrepareOutsideVehicleMenu(player, doFillMenuOutsideVehicle, context, worldobjects, test)
+end
+
+
+Events.OnFillWorldObjectContextMenu.Add(onfillMenuOutsideVehicleMenu)
